287d88a9eeb28e047e35db08baa6865e78ed27ac
Mert <mertrizakaradeniz@gmail.com>
2021-07-14 10:47:25 +0300

    Instruction fragment design & setup

--- app/src/main/java/com/example/foody/ui/instruction/InstructionsFragment.kt
+++ app/src/main/java/com/example/foody/ui/instruction/InstructionsFragment.kt
@@ -5,17 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.foody.R
+import android.webkit.WebViewClient
+import com.example.foody.data.model.Result
+import com.example.foody.databinding.FragmentInstructionsBinding
+import com.example.foody.util.Constants.RECIPE_RESULT_KEY
 
 
 class InstructionsFragment : Fragment() {
 
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    ): View {
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+        setupWebView()
+        return binding.root
     }
 
+    private fun setupWebView() {
+        val bundle: Result? = arguments?.getParcelable(RECIPE_RESULT_KEY)
+        binding.webViewInstruction.webViewClient = object : WebViewClient() {}
+        bundle?.sourceUrl?.let { binding.webViewInstruction.loadUrl(it) }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
